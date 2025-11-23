e236434d5487f0e52616172aa3d311af965645a9
WSmithDR <wsduenas@espol.edu.ec>
2025-11-22 22:40:17 -0500

    feat: añadido middleware para autenticacion de la ruta interna que me permite buscar cliente por id

--- customers-api/index.js
+++ customers-api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const customerRoutes = require('./src/routes/customer.routes');
 const healthRoutes = require('./src/routes/health.routes');
+const customerController = require('./src/controllers/customer.controller')
+const { validateServiceToken } = require('./src/middlewares/auth.middleware');
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use('/health', healthRoutes);
 
 app.use('/customers', customerRoutes);
+
+app.use('/internal/customers/:id', validateServiceToken, customerController.getCustomerById);
 
 app.listen(port, () => {
   console.log(`Customers API escuchando en puerto ${port}`);
---
+++ customers-api/src/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const validateServiceToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ success: false, error: 'Acceso denegado: Falta token' });
+  }
+
+  if (token !== process.env.SERVICE_TOKEN) {
+    return res.status(403).json({ success: false, error: 'Acceso prohibido: Token inválido' });
+  }
+
+  next();
+};
+
+module.exports = { validateServiceToken };
