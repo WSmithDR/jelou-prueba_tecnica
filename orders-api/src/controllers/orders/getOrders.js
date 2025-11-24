7b0560c0388deeae82ec05d9a91de46e7ff8f226
WSmithDR <wsduenas@espol.edu.ec>
2025-11-24 12:56:40 -0500

    fix: subiendo correcciones adicionales

--- orders-api/src/controllers/orders/getOrders.js
+++ orders-api/src/controllers/orders/getOrders.js
@@ -1,4 +1,4 @@
-const pool = require('../../config/db');
+const {pool} = require('../../config/index');
 
 const getOrders = async (req, res) => {
   try {
--- orders-api/src/middlewares/auth.middleware.js
+++ orders-api/src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config/index');
+const {config} = require('../config');
 
 const validateServiceToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
